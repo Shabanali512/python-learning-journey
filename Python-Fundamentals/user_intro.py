e62a0fc0f3b2bddb4d924c4faae599f85fe306e9
Shaban Ali <shabi9405@gmail.com>
2026-01-31 13:28:48 +0500

    Added user introduction program

---
+++ Python-Fundamentals/user_intro.py
@@ -0,0 +1,8 @@
+# User Introduction Program
+
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+height = input("Enter your Height (in feet): ")
+city = input("Enter your City: ")
+
+print(f"My name is {name}. I'm {age} years old. My height is {height} feet. I'm from {city}.")
